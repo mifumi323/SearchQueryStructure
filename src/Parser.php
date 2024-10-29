f1eefe2d31ce873581b98f2d2e14d135dbfbb804
mifumi323 <mifumi323@gmail.com>
2024-10-29 23:23:05 +0900

    字句解析
    Mifumi323\SearchPhraseParserのほぼコピペ

--- src/Parser.php
+++ src/Parser.php
@@ -8,7 +8,84 @@ class Parser
     {
     }
 
-    public function parse(string $query) : INode {
+    /**
+     * 解析します。
+     */
+    public function parse(string $query): INode
+    {
+        $symbols = $this->lexicalAnalyze($query);
         throw new \Exception('Not implemented.');
+    }
+
+    /**
+     * 字句解析
+     */
+    private function lexicalAnalyze(string $query): array
+    {
+        $lex_pattern = [
+            [
+                'symbol' => 'AND',
+                'pattern' => 'AND\b|&',
+            ],
+            [
+                'symbol' => 'OR',
+                'pattern' => 'OR\b|\|',
+            ],
+            [
+                'symbol' => 'NOT',
+                'pattern' => 'NOT\b|-',
+            ],
+            [
+                'symbol' => 'BRST',
+                'pattern' => '\(',
+            ],
+            [
+                'symbol' => 'BREN',
+                'pattern' => '\)',
+            ],
+            [
+                'symbol' => 'PHRASE',
+                'pattern' => '"(([^"]*("")?)*)"',
+                'exec' => $this->quotedPhrase(...),
+            ],
+            [
+                'symbol' => 'PHRASE',
+                'pattern' => '[^\s\(\)]+',
+            ],
+            [
+                'symbol' => '',
+                'pattern' => '\s+',
+            ],
+        ];
+        $lex = [];
+        $modifier = $this->ignore_case ? 'iu' : 'u';
+        do {
+            $matched = false;
+            foreach ($lex_pattern as $val) {
+                $pattern = '/^(?:'.$val['pattern'].')/'.$modifier;
+                if (preg_match($pattern, $query, $matches)) {
+                    if ($val['symbol'] !== '') {
+                        $exec = $val['exec'] ?? false;
+                        $lex[] = [
+                            'symbol' => $val['symbol'],
+                            'value' => is_callable($exec) ? $exec($matches) : $matches[0],
+                        ];
+                    }
+                    $query = mb_substr($query, mb_strlen($matches[0]));
+                    $matched = true;
+                    break;
+                }
+            }
+        } while ($matched);
+
+        return $lex;
+    }
+
+    /**
+     * クォーテーションで囲まれた文字列の中身をちょっといじる
+     */
+    private function quotedPhrase(array $matches): string
+    {
+        return str_replace('""', '"', $matches[1]);
     }
 }
